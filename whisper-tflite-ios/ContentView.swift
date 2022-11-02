13c7114b7bb0f5157fd87a184dc5fce82ec8bebd
Ben Nortier <bjnortier@gmail.com>
2022-11-02 17:11:05 +0200

    Initial Commit

---
+++ whisper-tflite-ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  whisper-tflite-ios
+//
+//  Created by Ben Nortier on 2022/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ whisper-tflite-ios/whisper_tflite_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_tflite_iosApp.swift
+//  whisper-tflite-ios
+//
+//  Created by Ben Nortier on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct whisper_tflite_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
